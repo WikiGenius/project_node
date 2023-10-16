d84fe580c0de1b26e29ffa1104c89c584f86a1ab
WikiGenius <muhammedelyamani92@gmail.com>
2023-10-16 21:38:30 +0300

    update frontend

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,8 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -10,6 +12,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // DELEGATION
 if (mapBox) {
@@ -17,7 +20,7 @@ if (mapBox) {
     const locations = JSON.parse(mapBox.dataset.locations);
     const mapboxToken = mapBox.dataset.mapboxToken;
     const mapboxStyle = mapBox.dataset.mapboxStyle;
-
+    
     if (!locations || !mapboxToken || !mapboxStyle) {
       throw new Error('Missing required map data');
     }
@@ -41,10 +44,12 @@ if (logOutBtn) logOutBtn.addEventListener('click', logout);
 if (userDataForm)
   userDataForm.addEventListener('submit', e => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const photo = document.getElementById('photo').files[0];
-    updateSettings({ name, email, photo }, 'data');
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+
+    updateSettings(form, 'data');
   });
 
 if (userPasswordForm)
@@ -65,3 +70,13 @@ if (userPasswordForm)
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
   });
+
+if (bookBtn)
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
+  });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
